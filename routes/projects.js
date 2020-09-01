33fffa8b8ff04133dad7a3fb3c12555fd0309bbb
mapletree111 <61851239+mapletree111@users.noreply.github.com>
2020-09-01 01:02:53 -0700

    Add links to other pages
    Added links to project page and redid the jade views to be use generic headers and footers for more modular layout

--- routes/projects.js
+++ routes/projects.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Kien Tran'});
+  res.render('projects', { title: 'Kien Tran - Projects'});
 });
-
 
 module.exports = router;
