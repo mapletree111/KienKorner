0b72ee1942a24e53e2032ab2bda441767f4241fb
mapletree111 <61851239+mapletree111@users.noreply.github.com>
2020-09-25 02:19:56 -0700

    Added tektronix description, fixed some UI issues

--- public/javascript/page.js
+++ public/javascript/page.js
@@ -5,3 +5,14 @@ AOS.init({
 $(window).on('beforeunload', function(){
     $(window).scrollTop(0);
 });
+
+$(window).on('load', function () {
+
+    $(".experienceButton").click(function() {
+        console.log("Fuck me")
+        $('html,body').animate({
+            scrollTop: $(".experienceSection").offset().top},
+            'slow');
+    });
+});
+
