c7eacc7e599057e8537087addc9d9f419adc7778
Kien Tran <kien.tran1995@gmail.com>
2020-11-24 22:44:11 -0800

    Added in project for Fire Predictor Application

--- public/javascript/page.js
+++ public/javascript/page.js
@@ -1,10 +1,11 @@
 AOS.init({
     duration: 1200,
 })
+var slideIndex = 1;
 
-$(window).on('beforeunload', function(){
-    $(window).scrollTop(0);
-});
+// $(window).on('beforeunload', function(){
+//     $(window).scrollTop(0);
+// });
 
 $(window).on('load', function () {
 
@@ -18,5 +19,34 @@ $(window).on('load', function () {
             scrollTop: $(".projectSection").offset().top},
             'slow');
     });
+    showSlides(slideIndex);
 });
+
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
